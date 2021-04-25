0f849cd2b28844b200bf23a94f0c36093820f854
freanymellynusmany <78341427+freanymellynusmany@users.noreply.github.com>
2021-04-25 19:21:54 +0700

    Add files via upload

---
+++ Labb pertemuan_10_Dictionary.py
@@ -0,0 +1,26 @@
+#71200644
+#Freany Mellyn Usmany
+#Universitas Kristen Duta wacana
+
+#Pada hari minggu Evi pergi jalan-jalan keluar kota dan Evi ingin mencoba kuliner dari berbagi kota yaitu 
+#kuliner dari kota jogja,jakarta,dan bandung.Kemudian Evi mencari informasi mengenai makanan khas tersebut dan evi 
+#dan evi akhirnya mengetahui kuliner-kuliner yang inginkan,namun Evi tidak bisa mengingat semuanya untuk itu buatlah 
+#program yang menjabarkan kuliner-kuliner apa saja pada daerah-daerah tersebut....
+
+# input
+("makanan_khas_jogja = gudek")
+("makanan_khas_jakarta = keraktelor")
+("makanan_khas_bandung = karedok")
+
+makanan_khas = {'jogja':'gudek', 'jakarta': 'kerak telor', 'bandung':'karedok'}
+
+#data
+makanan_khas_jogja = makanan_khas.get('jogja')
+makanan_khas_jakarta = makanan_khas.get('jakarta')
+makanan_khas_bandung = makanan_khas.get('bandung')
+
+print("makanan_khas_jogja: ", makanan_khas_jogja)
+print("makanan_khas_jakarta: ", makanan_khas_jakarta)
+print("makanan_khas_bandung: ", makanan_khas_bandung)
+
+
